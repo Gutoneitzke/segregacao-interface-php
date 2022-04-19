87f8da1b1a2dbc6ac10e0ba40ecd6abe63fe8268
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-04-19 13:58:10 -0300

    our interfaces with class

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+  interface Abelha
+  {
+      public function voar();
+  }
+
+  interface AbelharQuePicam extends Abelha
+  {
+      public function picar();
+  }
+
+  class Jatai implements Abelha
+  {
+      function __construct()
+      {
+          echo "Jataí: <br>";
+          $this->voar();
+      }
+      public function voar()
+      {
+          echo "Voa";
+      }
+  }
+
+  class Vespa implements AbelharQuePicam
+  {
+  	  function __construct()
+      {
+      	  echo "Vespa: <br>";
+          $this->voar();
+          $this->picar();
+      }
+      public function voar()
+      {
+          echo "Voa <br>";
+      }
+
+      public function picar()
+      {
+          echo "Pica";
+      }
+  }
+  
+  $abelha1 = new Vespa();
+  echo "<br><br>";
+  $abelha2 = new Jatai();
+?>
